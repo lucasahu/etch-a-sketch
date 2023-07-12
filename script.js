1f47f141474bd56fab2038c03f361b7f5817d554
Lucas <lucasignahumada@gmail.com>
2023-07-12 05:15:58 -0400

    Set up html structure and grid layout for the canvas

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Declaration
+const main = document.querySelector('div.main');
+const grid = document.querySelector('div.grid');
+//DOM Append
+
+//DOM Methods
+
+grid.className = 'grid';
+grid.style.gridTemplateColumns = "repeat(16, 1fr)";
+grid.style.gridTemplateRows = "repeat(16, 1fr)";
