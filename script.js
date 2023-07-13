68b382fe24bd017ec69a3b2f797762abf411972d
Lucas <lucasignahumada@gmail.com>
2023-07-13 16:44:34 -0400

    Add basic sketch functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Helper Functions
+
 // DOM Declaration
 const main = document.querySelector('div.main');
 const grid = document.querySelector('div.grid');
+let gridbox = ''
 //DOM Append
 
 //DOM Methods
@@ -8,3 +11,13 @@ const grid = document.querySelector('div.grid');
 grid.className = 'grid';
 grid.style.gridTemplateColumns = "repeat(16, 1fr)";
 grid.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+    let gridbox = document.createElement('div');
+    gridbox.addEventListener('mouseover', () => {
+        gridbox.style.backgroundColor = 'black';    
+    });
+    grid.insertAdjacentElement('beforeend', gridbox);
+}
+
+//DOM Event Listeners
